bedeb246c081be528fa50208df0212966a017e2a
prateek <prateekg@softcell.com>
2017-09-30 15:26:08 +0530

    adding profile constants for gonogo

---
+++ gateway/src/main/java/com/softcell/gonogo/gateway/constants/GoNoGoConstants.java
@@ -0,0 +1,12 @@
+package com.softcell.gonogo.gateway.constants;
+
+public class GoNoGoConstants {
+
+    // Spring profiles for development, test and production
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    // Spring profile used to disable swagger
+    public static final String SPRING_PROFILE_SWAGGER = "swagger";
+
+}
