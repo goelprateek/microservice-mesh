84b935b785d8c9759d0e8c9e6afa1bcae8c708b5
prateek <prateekg@softcell.com>
2017-10-01 19:38:32 +0530

    changing userInfoRestTemplate to take eureka serviceId to redirection to different microservice based on url

--- gateway/src/main/java/com/softcell/gonogo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/softcell/gonogo/gateway/GatewayApplication.java
@@ -4,18 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.cloud.client.loadbalancer.RetryLoadBalancerInterceptor;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
@@ -25,16 +22,18 @@ import org.springframework.security.oauth2.client.token.grant.password.ResourceO
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * API Gateway
  */
-@ComponentScan
-@EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
+
+@SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class GatewayApplication extends SpringBootServletInitializer{
@@ -67,15 +66,14 @@ public class GatewayApplication extends SpringBootServletInitializer{
 
 
 	@Bean
-	UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
-		return (OAuth2RestTemplate template) -> {
+	UserInfoRestTemplateCustomizer oauth2RestTemplateCustomizer(RetryLoadBalancerInterceptor interceptor) {
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(interceptor);
+		return template -> {
 			AccessTokenProviderChain accessTokenProviderChain = Stream
-					.of(
-							new AuthorizationCodeAccessTokenProvider(),
-							new ImplicitAccessTokenProvider(),
-							new ResourceOwnerPasswordAccessTokenProvider(),
-							new ClientCredentialsAccessTokenProvider())
-					.peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+					.of(new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
+							new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider())
+					.peek(tp -> tp.setInterceptors(interceptors))
 					.collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
 			template.setAccessTokenProvider(accessTokenProviderChain);
 		};
