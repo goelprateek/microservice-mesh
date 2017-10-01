ffe4cdecf236c21a32dd0e4bee813681001069d9
prateek <prateekg@softcell.com>
2017-10-01 19:44:15 +0530

    changing name of class and removing unused code

--- uaa-server/src/main/java/com/softcell/gonogo/uaaserver/config/security/UaaConfiguration.java
+++ uaa-server/src/main/java/com/softcell/gonogo/uaaserver/config/security/UaaConfiguration.java
@@ -42,7 +42,7 @@ import java.util.Arrays;
 @EnableWebSecurity
 @Order(2)
 @RefreshScope
-public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class UaaConfiguration extends AuthorizationServerConfigurerAdapter {
 
 
     @Autowired
@@ -60,6 +60,56 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Value("${gng.security.jwt.pass}")
     private String passKey;
 
+
+
+    /*@EnableResourceServer
+    public static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        private final TokenStore tokenStore;
+
+        private final CorsFilter corsFilter;
+
+        public ResourceServerConfiguration(TokenStore tokenStore, CorsFilter corsFilter) {
+            this.tokenStore = tokenStore;
+            this.corsFilter = corsFilter;
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .exceptionHandling()
+                    .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                    .and()
+                    .csrf()
+                    .disable()
+                    .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                    .headers()
+                    .frameOptions()
+                    .disable()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/api/register").permitAll()
+                    .antMatchers("/api/activate").permitAll()
+                    .antMatchers("/api/authenticate").permitAll()
+                    .antMatchers("/api/account/reset-password/init").permitAll()
+                    .antMatchers("/api/account/reset-password/finish").permitAll()
+                    .antMatchers("/api/profile-info").permitAll()
+                    .antMatchers("/api/**").authenticated()
+                    .antMatchers("/management/health").permitAll()
+                    .antMatchers("/management/**").hasAuthority("ROLE_ADMIN")
+                    .antMatchers("/v2/api-docs/**").permitAll()
+                    .antMatchers("/swagger-resources/configuration/ui").permitAll()
+                    .antMatchers("/swagger-ui/index.html").hasAuthority("ROLE_ADMIN");
+        }
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.resourceId("uaa-resource").tokenStore(tokenStore);
+        }
+    }*/
 
     @Override
     public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
@@ -98,10 +148,8 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
                 .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST)
                 .accessTokenConverter(accessTokenConverter())
                 .tokenEnhancer(tokenEnhancerChain)
-                //.userApprovalHandler(approvalHandler())
                 .authenticationManager(authenticationManager)
                 .userDetailsService(hybridUserDetailsService);
-                //.setClientDetailsService(clientDetailService)
         // @formatter:on
     }
 
@@ -148,12 +196,12 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     }
 
     @Bean
-    public OAuth2AccessDeniedHandler oAuth2AccessDeniedHandler(){
+    public OAuth2AccessDeniedHandler oAuth2AccessDeniedHandler() {
         return new OAuth2AccessDeniedHandler();
     }
 
     @Bean
-    public OAuth2AuthenticationEntryPoint oAuth2AuthenticationEntryPoint(){
+    public OAuth2AuthenticationEntryPoint oAuth2AuthenticationEntryPoint() {
         OAuth2AuthenticationEntryPoint oAuth2AuthenticationEntryPoint = new OAuth2AuthenticationEntryPoint();
         oAuth2AuthenticationEntryPoint.setRealmName("auth-server");
         return oAuth2AuthenticationEntryPoint;
